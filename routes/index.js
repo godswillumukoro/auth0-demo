c29683aaee84f67ac50f4773d5e40945abb3e468
Godswill Umukoro <umukro6@gmail.com>
2023-05-15 13:50:49 +0100

    protected route

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const { requiresAuth } = require("express-openid-connect");
 
 router.get("/", (req, res) => {
-  res.render("index", {
-    title: "Node.js",
+  if (req.oidc.isAuthenticated()) {
+    res.render("admin", {
+      title: "Node.js",
+      isAuthenticated: req.oidc.isAuthenticated(),
+      user: req.oidc.user,
+    });
+  } else {
+    res.render("index", {
+      title: "Node.js",
+      isAuthenticated: req.oidc.isAuthenticated(),
+      user: req.oidc.user,
+    });
+  }
+  console.log(req.oidc.isAuthenticated());
+});
+
+router.get("/app-admin", requiresAuth(), (req, res) => {
+  res.render("admin", {
+    title: "Admin Page",
     isAuthenticated: req.oidc.isAuthenticated(),
-    user: req.oidc.user
+    user: req.oidc.user,
   });
   console.log(req.oidc.isAuthenticated());
 });
